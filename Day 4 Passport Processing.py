41e5ab215f2b46179f6da8d50e42960e82bd2d23
Hector Mata <hrmb26@gmail.com>
2021-03-08 18:36:54 -0400

    refactored code

--- Day 4 Passport Processing.py
+++ Day 4 Passport Processing.py
@@ -2,26 +2,13 @@
 
 def doubleCheck(input):
     doublechecked_passports = 0    
+
     for item in input:
-        validByr = False
-        validIyr = False
-        validEyr = False
-        validHgt = False
-        validHcl = False
-        validPid = False
-        validEyr = False
-        if 'byr' in item and 1920 <= int(item["byr"]) <= 2002:
-            validByr = True            
-        else:
-            validByr = False
-        if 'iyr' in item and 2010 <= int(item["iyr"]) <= 2020:
-            validIyr = True            
-        else:
-            validIyr = False
-        if 'eyr' in item and 2020 <= int(item["eyr"]) <= 2030:
-            validEyr = True            
-        else:
-            validEyr = False
+        validByr = 'byr' in item and 1920 <= int(item["byr"]) <= 2002
+        validIyr = 'iyr' in item and 2010 <= int(item["iyr"]) <= 2020
+        validEyr = 'eyr' in item and 2020 <= int(item["eyr"]) <= 2030
+        validEcl = 'ecl' in item and item['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+
         if 'hgt' in item and item['hgt'][-2:] == 'cm':
             if 150 <= int(item['hgt'][:-2]) <= 193:
                 validHgt = True                
@@ -32,6 +19,9 @@ def doubleCheck(input):
                 validHgt = True
             else:
                 validHgt = False
+        else:
+            validHgt = False
+
         if 'hcl' in item and len(item['hcl']) == 7 and item['hcl'][:1] == '#':
             for character in item['hcl'][1:].lower():
                 if character not in '0123456789abcdef':
@@ -41,10 +31,7 @@ def doubleCheck(input):
                     validHcl = True
         else:
             validHcl = False
-        if 'ecl' in item and item['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
-            validEcl = True            
-        else:
-            validEcl = False
+
         if 'pid' in item and len(item['pid']) == 9:
             for digit in item['pid']:
                 if digit in '0123456789':
@@ -54,20 +41,21 @@ def doubleCheck(input):
                     break
         else:
             validPid = False
+
         if (validByr and validEcl and validEyr and validHcl and validHgt and validIyr and validPid):
             doublechecked_passports += 1
+
     return doublechecked_passports
-            
 
 def checkPassports(passports):
     fields = ['byr', 'iyr','eyr','hgt','hcl','ecl','pid']
     validPassports = 0
-    for x in passports:        
+    for x in passports:
         if 'cid' not in x:
             if all(elem in x for elem in fields):
                 validPassports += 1
         elif all(elem in x for elem in fields):
-            validPassports += 1        
+            validPassports += 1
     return validPassports
 
 def dictionary_convert(pass_list):
@@ -80,7 +68,7 @@ def dictionary_convert(pass_list):
     return dictionary
 
 with open('./files/day4.txt') as raw_input:
-    split_1 = raw_input.read().split('\n\n')  # divide cada pasaporte con doble salto de linea
+    split_1 = raw_input.read().split('\n\n') # divide cada pasaporte con doble salto de linea
     split_2 = [string.replace('\n',' ') for string in split_1] # reemplaza los salto de linea por espacio
     split_3 = [string.split() for string in split_2] # separa cada item de cada pasaporte
     passports = [dictionary_convert(item) for item in split_3] # transforma la lista en un diccionario
